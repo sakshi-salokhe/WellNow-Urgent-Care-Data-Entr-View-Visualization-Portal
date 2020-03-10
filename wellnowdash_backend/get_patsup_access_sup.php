b926c0e98e0c9546058cbfef1281ab62e2b4d320
sakshi-salokhe <sakshisalokhe1@gmail.com>
2020-03-10 16:44:21 -0400

    pat sup supervisor privileges

---
+++ wellnowdash_backend/get_patsup_access_sup.php
@@ -0,0 +1,63 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: PUT, GET, POST");
+require "connect.php";
+session_start();
+
+$emp = [];
+
+$sup_id = $_GET['sup_id'];
+
+$sql1 = "select * from users where user_id = '$sup_id'";
+$res1 = mysqli_query($con, $sql1);
+$row1 = mysqli_fetch_assoc($res1);
+$dashboards = $row1['dashboards'];
+
+
+
+$sql = "select * from users where isManager = 0 and dashboards = 4";
+if($res = mysqli_query($con, $sql))
+{
+	$c = 0;
+	while($row = mysqli_fetch_assoc($res))
+	{
+		$uid = $row['user_id'];
+		$q = "select * from pat_sup_access where user_id = '$uid'";
+		
+		$result = mysqli_query($con, $q);
+		$row_dash = mysqli_fetch_assoc($result);
+		
+		$username = $row['fullname'];
+		$isActive = $row['isActive'];
+		
+		$emp[$c]['userid'] = $uid;
+		$emp[$c]['fullname'] = $username;
+		$emp[$c]['supervisor_id'] = $sup_id;
+		
+		$emp[$c]['saf_mvp_sod'] = $row_dash['saf_mvp_sod'];
+		$emp[$c]['saf_inval_addr_sod'] = $row_dash['saf_inval_addr_sod'];
+		$emp[$c]['ash_attachments_sod'] = $row_dash['ash_attachments_sod'];
+		$emp[$c]['ash_wc_mailing_sod'] = $row_dash['ash_wc_mailing_sod'];
+		$emp[$c]['ash_wc_deleted_sod'] = $row_dash['ash_wc_deleted_sod'];
+		$emp[$c]['ash_acc_type_sod'] = $row_dash['ash_acc_type_sod'];
+		$emp[$c]['ash_last_addr_sod'] = $row_dash['ash_last_addr_sod'];
+		$emp[$c]['bailey_indep_health_sod'] = $row_dash['bailey_indep_health_sod'];
+		$emp[$c]['bailey_bcbs_sod'] = $row_dash['bailey_bcbs_sod'];
+		$emp[$c]['bailey_emails_sod'] = $row_dash['bailey_emails_sod'];
+		$emp[$c]['justin_ndc_num_sod'] = $row_dash['justin_ndc_num_sod'];
+		$emp[$c]['justin_medicare_loc_sod'] = $row_dash['justin_medicare_loc_sod'];
+		$emp[$c]['justin_medicare_sec_sod'] = $row_dash['justin_medicare_sec_sod'];
+		
+		$emp[$c]['isActive'] = $isActive;
+		
+		$c++;
+	}
+	echo json_encode($emp);
+}
+else
+{
+	http_response_code(404);
+}
+
+?>
