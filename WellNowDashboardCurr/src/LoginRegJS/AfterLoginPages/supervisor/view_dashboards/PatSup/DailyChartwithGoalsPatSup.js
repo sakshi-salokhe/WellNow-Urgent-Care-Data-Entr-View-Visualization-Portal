9d5192425d3e3c872d050a4df12d381baf8c2a15
sakshi-salokhe <sakshisalokhe1@gmail.com>
2020-03-17 10:59:54 -0400

    Add files via upload

---
+++ WellNowDashboardCurr/src/LoginRegJS/AfterLoginPages/supervisor/view_dashboards/PatSup/DailyChartwithGoalsPatSup.js
@@ -0,0 +1,176 @@
+import React, {Component} from "react";
+import ReactDOM from "react-dom"
+import axios from "axios"
+import qs from "qs"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import {Bar, Line} from 'react-chartjs-2';
+
+import IndexHeader from "../../../../../IndexHeader";
+import PrevDataVisualise from '../PrevDataVisualise'
+
+class DailyChartwithGoalsPatSup extends Component
+{
+	constructor(props)
+	{
+		super(props);
+		this.back = this.back.bind(this);
+		
+		var ds = [];
+		var num = this.props.data.length;
+		
+		var dlabel = [];
+		var dlabel1 = [];
+		var d_temp = [];
+		var d1 = [];
+		var d2 = [];
+		
+		var dynamicColors = function() {
+			var r = Math.floor(Math.random() * 255);
+			var g = Math.floor(Math.random() * 255);
+			var b = Math.floor(Math.random() * 255);
+			var opacity = 0.4;
+			return "rgb(" + r + "," + g + "," + b +  "," + opacity +")";
+		}
+		
+		for(var i=0; i< num-1; i++)
+		{
+			for(var key in this.props.data[i])
+			{
+				//dlabel.push(key);
+				d_temp.push(this.props.data[i][key]);
+			}
+			d_temp.shift();
+			d_temp.shift();
+			d_temp.shift();
+			d_temp.shift();
+			d_temp.pop();
+			
+			/*dlabel.shift();
+			dlabel.shift();
+			dlabel.shift();
+			dlabel.shift();
+			dlabel.pop();*/
+			
+			console.log("new array for data:",d_temp);
+			
+			var len = d_temp.length;
+			
+			for(var k = 0; k < (len/2); k++)
+			{
+				d1.push(d_temp[k]);
+				//dlabel1.push(dlabel[k]);
+			}
+			
+			for(var k = (len/2); k < (len); k++)
+			{
+				d2.push(d_temp[k]);
+			}
+			
+			console.log("d1", d1);
+			console.log("d2", d2);
+			
+			ds.push({
+			label: this.props.data[i].when_done,
+			fill: true,
+			linetension: 0.1,
+			borderCapStyle: 'butt',
+			borderDash: [],
+			borderDashOffset: 0.0,
+			borderJoinStyle:  'miter',
+			pointBorderWidth: 10,
+			pointBorderColor: 'rgba(0,0,0,1)',
+			pointHoverRadius: 5,
+			pointHoverBorderWidth: 2,
+			pointHoverBorderColor: 'rgba(0,0,0,1)',
+			data:d1,
+			backgroundColor: dynamicColors(),
+			pointRadius: 15,
+			pointHitRadius: 10,
+			order: 2
+			},
+			{
+				label: this.props.data[i].mon,
+				fill: false,
+				linetension: 0.5,
+				borderCapStyle: 'butt',
+				borderDash: [],
+				borderDashOffset: 0.0,
+				borderJoinStyle:  'miter',
+				pointBorderWidth: 10,
+				pointHoverRadius: 5,
+				pointHoverBorderWidth: 2,
+				pointHoverBorderColor: 'rgba(0, 0, 0)',
+				data:d2,
+				type: 'bubble',
+				backgroundColor:dynamicColors(),
+				pointRadius: 5,
+				pointHitRadius: 10,
+				order: 1
+			});
+			
+			dlabel = [];
+			d1 = [];
+			d2 = [];
+			d_temp = [];
+		}
+		
+		for(var key in this.props.data[0])
+		{
+			dlabel.push(key);
+		}	
+		dlabel.shift();
+		dlabel.shift();
+		dlabel.shift();
+		dlabel.shift();
+		dlabel.pop();
+		
+		for(var k = 0; k < (dlabel.length/2); k++)
+		{
+			dlabel1.push(dlabel[k]);
+		}
+			
+				
+		this.state = {
+			chartData:{
+				labels : dlabel1,
+				datasets : ds
+			}
+		}
+	}
+	
+	back()
+	{
+		ReactDOM.render(<PrevDataVisualise user_id = {this.props.data[0].supervisor_id}/>, document.getElementById('root'));
+	}
+	
+	render()
+	{
+		
+		return(
+			<div className = "container">
+				<br />
+				<br />
+				<br />
+				<div className="form-group">
+					<div className="col-lg-10 col-sm-10 col-md-10 col-xs-10">
+						<button type="button" className="btn btn-primary" onClick={this.back}> Back </button>
+					</div>
+					
+					<div className="col-lg-2 col-sm-2 col-md-2 col-xs-2"> </div>
+						
+				</div>
+				
+				<br />
+				<br />
+				<div classname = "chart">
+					<Bar data = {this.state.chartData} options = {{
+						title:{ display: true, text: 'Patient Support Dashboard Data Visualisation for today'}
+					}} />
+				</div>
+			</div>
+		)
+	}
+}
+
+export default DailyChartwithGoalsPatSup
---
+++ WellNowDashboardCurr/src/LoginRegJS/AfterLoginPages/supervisor/view_dashboards/PatSup/PatSup_Goals_headers.js
@@ -0,0 +1,91 @@
+import React, {Component} from "react";
+import ReactDOM from "react-dom"
+import axios from "axios"
+import qs from "qs"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import moment from "moment"
+
+import IndexHeader from "../../../../../IndexHeader";
+import ViewDashboards from "../../ViewDashboards"
+import GoalsViewPatSup from './GoalsViewPatSup'
+
+class PatSup_Goals_headers extends Component
+{
+	constructor(props)
+	{
+		super(props);
+		this.state = 
+		{
+			employees: []
+		};
+		this.back = this.back.bind(this);
+	}
+	userList()
+	{
+		return this.props.data.map(function(object)
+		{
+			return <GoalsViewPatSup key={object.id} obj={object} />;
+		});
+	}
+	
+	back()
+	{
+		console.log("here:back: ",this.props.data[0].userid);
+		ReactDOM.render(<ViewDashboards user_id = {this.props.data[0].userid}/>, document.getElementById('root'));
+	}
+	
+	render()
+	{
+		return (
+			<div className="container-fluid">
+				
+				<IndexHeader />
+				
+				<div>
+					<div className= "row">
+						<div className = "col-lg-3 col-md-3 col-sm-3 col-cs-3">
+							<button className="btn btn-primary" onClick={this.back}> Back </button>
+						</div>
+						<div className = "col-lg-9 col-md-9 col-sm-9 col-cs-9">
+							<h3 align="center"> Goals for OS Dashboard </h3>
+						</div>
+					</div>
+					<table className="table table-striped table-bordered" style={{marginTop: 20}}>
+						<thead>
+							<tr>
+								<th> </th>
+								<th colSpan="2"> Safiyyah </th>
+								<th colSpan="5"> Ashley Standish </th>
+								<th colSpan="3"> Bailea </th>
+								<th colSpan="3"> Justin </th>
+							</tr>
+							<tr>
+								<th> Date</th>
+								<th> MVP </th>
+								<th> Invalid Address (oldest date open) </th>
+								<th> Attachments </th>
+								<th> WC Mailing (yes/no) </th>
+								<th> WC deleted claims to archived (yes/no) </th>
+								<th> Accident Type (WC/NF) (yes/no) </th>
+								<th> Last Address given Worked (last date finished) </th>
+								<th> Indep Health </th>
+								<th> BCBS of WNY </th>
+								<th> Emails </th>
+								<th> NDC Numbers </th>
+								<th> Medicare Facility Location </th>
+								<th> Medicare Secondary to be original </th>
+							</tr>
+						</thead>
+						<tbody>
+							{this.userList()}
+						</tbody>
+					</table>
+				</div>
+				
+			</div>
+		)
+	}
+}
+
+export default PatSup_Goals_headers
