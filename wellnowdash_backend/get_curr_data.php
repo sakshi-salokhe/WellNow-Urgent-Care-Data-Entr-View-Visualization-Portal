ece1e25f11ae6cf12c28c8054fe22ad090b1464b
sakshi_salokhe <sakshisalokhe1@gmail.com>
2020-03-19 12:46:58 -0400

    todays data admin patsup

--- wellnowdash_backend/get_curr_data.php
+++ wellnowdash_backend/get_curr_data.php
@@ -127,6 +127,38 @@ else if($dashboards == '3' or $dashboards == 3)
 	}
 	
 }
+else if($dashboards == '4' or $dashboards == 4)
+{
+	$sql = "select * from pat_sup_data where when_done = '$today'";
+	if($res = mysqli_query($con, $sql))
+	{
+		$c = 0;
+		while($row = mysqli_fetch_assoc($res))
+		{
+			$emp[$c]['id'] = $row['pat_sup_id'];
+			$emp[$c]['when_done'] = $row['when_done'];
+			
+			$emp[$c]['saf_mvp_sod'] = $row['saf_mvp_sod'];
+			$emp[$c]['saf_inval_addr_sod'] = $row['saf_inval_addr_sod'];
+			$emp[$c]['ash_attachments_sod'] = $row['ash_attachments_sod'];
+			$emp[$c]['ash_wc_mailing_sod'] = $row['ash_wc_mailing_sod'];
+			$emp[$c]['ash_wc_deleted_sod'] = $row['ash_wc_deleted_sod'];
+			$emp[$c]['ash_acc_type_sod'] = $row['ash_acc_type_sod'];
+			$emp[$c]['ash_last_addr_sod'] = $row['ash_last_addr_sod'];
+			$emp[$c]['bailey_indep_health_sod'] = $row['bailey_indep_health_sod'];
+			$emp[$c]['bailey_bcbs_sod'] = $row['bailey_bcbs_sod'];
+			$emp[$c]['bailey_emails_sod'] = $row['bailey_emails_sod'];
+			$emp[$c]['justin_ndc_num_sod'] = $row['justin_ndc_num_sod'];
+			$emp[$c]['justin_medicare_loc_sod'] = $row['justin_medicare_loc_sod'];
+			$emp[$c]['justin_medicare_sec_sod'] = $row['justin_medicare_sec_sod'];
+			
+			$c++;
+		}
+		
+			echo json_encode($emp);
+	}
+	
+}
 else
 {
 	http_response_code(404);
